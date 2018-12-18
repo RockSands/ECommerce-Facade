e6a06157b6293071f1cb11b5fcd36abf17d066ef
RockSands <117019049@qq.com>
2018-12-18 16:32:06 +0800

    更新

--- EC-Registry/src/main/java/com/ec/registry/EC_Regidtry.java
+++ EC-Registry/src/main/java/com/ec/registry/EC_Regidtry.java
@@ -1,17 +1,17 @@
 package com.ec.registry;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @EnableEurekaServer 启动Eureka服务注册服务
  */
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class EC_Regidtry {
 	// 启动后访问Eureka服务页面，
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(EC_Regidtry.class).web(true).run(args);
+		SpringApplication.run(EC_Regidtry.class);
 	}
 }
