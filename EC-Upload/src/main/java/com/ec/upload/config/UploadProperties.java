c0c49889d0b281724ebe6f65a3d1dd7bb031e19c
RockSand <117019049@qq.com>
2018-12-26 23:35:45 +0800

    增加上传组件

---
+++ EC-Upload/src/main/java/com/ec/upload/config/UploadProperties.java
@@ -0,0 +1,21 @@
+package com.ec.upload.config;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+/**
+ * @author bystander
+ * @date 2018/9/16
+ */
+@Data
+@ConfigurationProperties(prefix = "ecommerce.upload")
+@Component
+public class UploadProperties {
+
+    private String baseUrl;
+    private List<String> allowTypes;
+}
---
+++ EC-Upload/src/main/java/com/ec/upload/web/UploadController.java
@@ -0,0 +1,35 @@
+package com.ec.upload.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ec.upload.service.UploadService;
+
+/**
+ * @author bystander
+ * @date 2018/9/16
+ */
+@RestController
+@RequestMapping("upload")
+public class UploadController {
+
+    @Autowired
+    private UploadService uploadService;
+
+
+    /**
+     * 图片的上传
+     *
+     * @param file
+     * @return
+     */
+    @PostMapping("image")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok(uploadService.uploadImage(file));
+    }
+}
