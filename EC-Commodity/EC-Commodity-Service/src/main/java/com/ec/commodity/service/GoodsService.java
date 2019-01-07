742cbf36e4e8611d6ef56e8c5fa12723d0c4db31
RockSands <117019049@qq.com>
2019-01-07 11:43:43 +0800

    修正错误

--- EC-Commodity/EC-Commodity-Service/src/main/java/com/ec/commodity/service/GoodsService.java
+++ EC-Commodity/EC-Commodity-Service/src/main/java/com/ec/commodity/service/GoodsService.java
@@ -13,23 +13,24 @@ import com.ec.common.vo.PageResult;
  * @date 2018/9/18
  */
 public interface GoodsService {
-    PageResult<Spu> querySpuByPage(Integer page, Integer rows, String key, Boolean saleable);
+	PageResult<Spu> querySpuByPage(Integer page, Integer rows, String key, Boolean saleable);
 
-    SpuDetail querySpuDetailBySpuId(Long spuId);
+	SpuDetail querySpuDetailBySpuId(Long spuId);
 
-    List<Sku> querySkuBySpuId(Long spuId);
+	List<Sku> querySkuBySpuId(Long spuId);
 
-    void deleteGoodsBySpuId(Long spuId);
+	void deleteGoodsBySpuId(Long spuId);
 
-    void addGoods(Spu spu);
+	void addGoods(Spu spu);
 
-    void updateGoods(Spu spu);
+	void updateGoods(Spu spu);
 
-    void handleSaleable(Spu spu);
+	void handleSaleable(Spu spu);
 
-    Spu querySpuBySpuId(Long spuId);
+	Spu querySpuBySpuId(Long spuId);
 
-    List<Sku> querySkusByIds(List<Long> ids);
+	List<Sku> querySkusByIds(List<Long> ids);
 
-    void decreaseStock(List<CartDto> cartDtos);
+	void decreaseStock(List<CartDto> cartDtos);
+
 }
