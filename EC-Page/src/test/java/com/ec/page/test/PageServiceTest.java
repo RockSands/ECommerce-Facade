5a12c1f9cef360902dfad7a7ff0d7076a717cec5
RockSand <117019049@qq.com>
2019-01-19 16:00:26 +0800

    增加

---
+++ EC-Page/src/test/java/com/ec/page/test/PageServiceTest.java
@@ -0,0 +1,26 @@
+package com.ec.page.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ec.page.service.PageService;
+
+/**
+ * @author bystander
+ * @date 2018/9/26
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PageServiceTest {
+
+	@Autowired
+	private PageService pageService;
+
+	@Test
+	public void createHtml() {
+		pageService.createHtml(156L);
+	}
+}
