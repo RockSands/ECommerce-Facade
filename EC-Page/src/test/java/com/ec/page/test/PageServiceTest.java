aeec719dd92f9edae3b779abd312dfb2316e4a8a
RockSands <117019049@qq.com>
2019-01-21 11:12:46 +0800

    去除黄色叹号

--- EC-Page/src/test/java/com/ec/page/test/PageServiceTest.java
+++ EC-Page/src/test/java/com/ec/page/test/PageServiceTest.java
@@ -1,6 +1,5 @@
 package com.ec.page.test;
 
-import org.assertj.core.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
