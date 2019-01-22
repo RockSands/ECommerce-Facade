807973221154b3ff56589b030e2c87c617469a60
RockSands <117019049@qq.com>
2019-01-22 17:36:06 +0800

    修正逻辑

---
+++ EC-User/EC-User-Service/src/main/java/com/ec/user/ECUserApplication.java
@@ -0,0 +1,21 @@
+package com.ec.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @author bystander
+ * @date 2018/9/29
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(value = "com.leyou.user.mapper")
+public class ECUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECUserApplication.class);
+    }
+}
---
+++ EC-User/EC-User-Service/src/main/java/com/ec/user/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.ec.user.mapper;
+
+import com.ec.user.pojo.User;
+
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author bystander
+ * @date 2018/9/29
+ */
+public interface UserMapper extends Mapper<User> {
+}
