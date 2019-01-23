83fd3e9b273347ec1474d9236719137594aa8ed2
RockSands <117019049@qq.com>
2019-01-23 18:52:16 +0800

    修改问题

--- EC-Cart/src/main/java/com/ec/cart/service/CartService.java
+++ EC-Cart/src/main/java/com/ec/cart/service/CartService.java
@@ -25,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CartService {
 
-    private static final String KEY_PREFIX = "ly:cart:uid:";
+    private static final String KEY_PREFIX = "ec:cart:uid:";
     @Autowired
     private StringRedisTemplate redisTemplate;
 
