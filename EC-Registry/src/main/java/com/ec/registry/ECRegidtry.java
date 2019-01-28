4ef87fe96623724ca1880ed877ea6cdbf37aee42
RockSands <117019049@qq.com>
2019-01-28 14:30:34 +0800

    修改

--- EC-Registry/src/main/java/com/ec/registry/ECRegidtry.java
+++ EC-Registry/src/main/java/com/ec/registry/ECRegidtry.java
@@ -9,9 +9,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EC_Regidtry {
+public class ECRegidtry {
 	// 启动后访问Eureka服务页面，
 	public static void main(String[] args) {
-		SpringApplication.run(EC_Regidtry.class);
+		SpringApplication.run(ECRegidtry.class);
 	}
 }
