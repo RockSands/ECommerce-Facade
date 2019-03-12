6a52066bcf159606a03966c7cfafc73de556bf72
RockSands <117019049@qq.com>
2019-03-12 10:33:33 +0800

    增加注释

--- EC-Pay/src/main/java/com/ec/pay/config/IdWorkerConfig.java
+++ EC-Pay/src/main/java/com/ec/pay/config/IdWorkerConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 import com.ec.common.utils.IdWorker;
 
+/**
+ * 雪花算法计算ID
+ * 分布式自增长
+ * @author Administrator
+ *
+ */
 @Configuration
 @EnableConfigurationProperties(IdWorkerProperties.class)
 public class IdWorkerConfig {
