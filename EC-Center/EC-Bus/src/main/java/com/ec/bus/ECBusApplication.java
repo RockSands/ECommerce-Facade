8185269228aa09c72476a3a483c6c6403eac6ba7
chenkw <117019049@qq.com>
2019-09-27 01:28:24 +0800

    修改

---
+++ EC-Center/EC-Bus/src/main/java/com/ec/bus/ECBusApplication.java
@@ -0,0 +1,15 @@
+package com.ec.bus;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * 启动类
+ * @author bystander
+ * @date 2018/9/30
+ */
+public class ECBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECBusApplication.class, args);
+    }
+}
