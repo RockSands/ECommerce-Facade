2423b0828941aded3d4e377975cda053c76ea850
chenkw <117019049@qq.com>
2019-09-30 16:25:17 +0800

    center暂时完成

--- EC-Center/EC-Registry/src/main/java/com/ec/registry/ECRegidtry.java
+++ EC-Center/EC-Registry/src/main/java/com/ec/registry/ECRegidtry.java
@@ -2,10 +2,13 @@ package com.ec.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @EnableEurekaServer 启动Eureka服务注册服务
+ * 
+ * 由于是服务注册中心,因而不能动态通过bus+config动态加载
  */
 @SpringBootApplication
 @EnableEurekaServer
